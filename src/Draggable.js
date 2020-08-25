e397c410ed61254f601e5da89d79c0d6699345b5
danialeshete <danial.eshete@gmail.com>
2020-08-25 20:35:48 +0200

    2 files modified, 1 added

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as d3 from "d3";
 import { FormControl, Button } from "react-bootstrap";
+import "./Draggable.css";
 
 const Draggable = () => {
   //Width and height
@@ -22,7 +23,7 @@ const Draggable = () => {
       { name: "Jerry" }
     ],
     edges: [
-      /* { source: 0, target: 1 },
+      { source: 0, target: 1 },
       { source: 0, target: 2 },
       { source: 0, target: 3 },
       { source: 0, target: 4 },
@@ -34,7 +35,9 @@ const Draggable = () => {
       { source: 5, target: 9 },
       { source: 6, target: 7 },
       { source: 7, target: 8 },
-      { source: 8, target: 9 } */
+      { source: 8, target: 9 }
+      /* 
+      { source: 0, target: 0 },
       { source: 0, target: 1 },
       { source: 0, target: 2 },
       { source: 0, target: 3 },
@@ -43,24 +46,11 @@ const Draggable = () => {
       { source: 0, target: 6 },
       { source: 0, target: 7 },
       { source: 0, target: 8 },
-      { source: 0, target: 9 }
+      { source: 0, target: 9 }  */
     ]
   };
 
-  //Initialize a simple force layout, using the nodes and edges in dataset
-  var force = d3
-    .forceSimulation(dataset.nodes)
-    .force("charge", d3.forceManyBody())
-    .force("link", d3.forceLink(dataset.edges))
-    .force(
-      "center",
-      d3
-        .forceCenter()
-        .x(w / 2)
-        .y(h / 2)
-    );
-
-  var colors = d3.scaleOrdinal(d3.schemeCategory10);
+  var x = dataset.nodes.length;
 
   //Create SVG element
   var svg = d3
@@ -69,101 +59,33 @@ const Draggable = () => {
     .attr("width", w)
     .attr("height", h);
 
-  //Create edges as lines
-  var edges = svg
-    .selectAll("line")
-    .data(dataset.edges)
-    .enter()
-    .append("line")
-    .style("stroke", "#ccc")
-    .style("stroke-width", 1);
+  start(dataset);
 
-  //Create nodes as circles
-  var nodes = svg
-    .selectAll("circle")
-    .data(dataset.nodes)
-    .enter()
-    .append("circle")
-    .attr("r", 10)
-    .style("fill", function(d, i) {
-      return colors(i);
-    })
-    .call(
-      d3
-        .drag() //Define what to do on drag events
-        .on("start", dragStarted)
-        .on("drag", dragging)
-        .on("end", dragEnded)
-    );
+  function start(dataset) {
+    var force = d3
+      .forceSimulation(dataset.nodes)
+      .force("charge", d3.forceManyBody())
+      .force("link", d3.forceLink(dataset.edges))
+      .force(
+        "center",
+        d3
+          .forceCenter()
+          .x(w / 2)
+          .y(h / 2)
+      );
 
-  //Add a simple tooltip
-  nodes.append("title").text(function(d) {
-    return d.name;
-  });
-
-  //Every time the simulation "ticks", this will be called
-  force.on("tick", function() {
-    edges
-      .attr("x1", function(d) {
-        return d.source.x;
-      })
-      .attr("y1", function(d) {
-        return d.source.y;
-      })
-      .attr("x2", function(d) {
-        return d.target.x;
-      })
-      .attr("y2", function(d) {
-        return d.target.y;
-      });
-
-    nodes
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      });
-  });
-
-  //Define drag event functions
-  function dragStarted(d) {
-    if (!d3.event.active) force.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-
-  function dragging(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
-
-  function dragEnded(d) {
-    if (!d3.event.active) force.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-
-  /*   $(el).bind(“updateD3”, function() { 
-   console.log('Updating …');
-  })
-  $(el).trigger("updateD3"); // Updating ... */
-
-  var x = dataset.nodes.length;
-  var array = Array();
-  console.log(dataset.nodes);
-  console.log(dataset.edges);
-  console.log(dataset.nodes.length);
-
-  function add_element_to_nodes() {
-    var text = document.getElementById("text1").value;
-    console.log("text1: " + text);
-    dataset.nodes.push({
-      name: text,
-      index: x
-    });
-
-    svg
+    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+    //Create edges as lines
+    var edges = svg
+      .selectAll("line")
+      .data(dataset.edges)
+      .enter()
+      .append("line")
+      .style("stroke", "#ccc")
+      .style("stroke-width", 1);
+    console.log(edges);
+    //Create nodes as circles
+    var nodes = svg
       .selectAll("circle")
       .data(dataset.nodes)
       .enter()
@@ -179,62 +101,89 @@ const Draggable = () => {
           .on("drag", dragging)
           .on("end", dragEnded)
       );
+      
+
+      //Every time the simulation "ticks", this will be called
+      force.on("tick", function() {
+        edges
+          .attr("x1", function(d) {
+            return d.source.x;
+          })
+          .attr("y1", function(d) {
+            return d.source.y;
+          })
+          .attr("x2", function(d) {
+            return d.target.x;
+          })
+          .attr("y2", function(d) {
+            return d.target.y;
+          });
+
+        nodes
+          .attr("cx", function(d) {
+            return d.x;
+          })
+          .attr("cy", function(d) {
+            return d.y;
+          });
+      });
+
+      //Add a simple tooltip
+      nodes.append("title").text(function(d) {
+        return d.name;
+      });
+    
+
+    //Define drag event functions
+        function dragStarted(d) {
+      if (!d3.event.active) force.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragging(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragEnded(d) {
+      if (!d3.event.active) force.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+  /*   $(el).bind(“updateD3”, function() { 
+   console.log('Updating …');
+  })
+  $(el).trigger("updateD3"); // Updating ... */
+
+  function addTopic() {
+    var topicText = document.getElementById("topicTextId").value;
+    dataset.nodes.push({
+      name: topicText
+    });
+    console.log("Nodes: ");
+    console.log(dataset.nodes);
+    document.getElementById("topicTextId").value = "";
+
     dataset.edges.push({
       source: dataset.nodes[0],
-      target: dataset.nodes[x]
+      target: dataset.nodes[dataset.nodes.length - 1]
     });
-    x++;
-    svg
-      .selectAll("line")
-      .data(dataset.edges)
-      .enter()
-      .append("line")
-      .style("stroke", "#ccc")
-      .style("stroke-width", 1);
-    document.getElementById("text1").value = "";
-    console.log(dataset.nodes);
+    console.log("Edges: ");
     console.log(dataset.edges);
 
-    /*     var e = "<hr/>";
-
-    for (var y = 0; y < dataset.nodes.length; y++) {
-      e += "Element " + y + " = " + dataset.nodes[y] + "<br/>";
-    }
-    document.getElementById("Result").innerHTML = e; */
-  }
-  function add_edges_to_nodes() {
-    dataset.edges[x] = document.getElementById("text1").value;
-    console.log("Element: " + dataset.edges[x] + " Added at index " + x);
-    x++;
-    document.getElementById("text1").value = "";
-    /*     var e = "<hr/>";
-
-    for (var y = 0; y < nodes.length; y++) {
-      e += "Element " + y + " = " + nodes[y] + "<br/>";
-    }
-    document.getElementById("Result").innerHTML = e; */
+    start(dataset);
   }
 
   return (
     <div>
-      <FormControl type="text" id="text1" />
-      <FormControl
-        type="button"
-        id="button1"
-        value="Add"
-        onClick={add_element_to_nodes}
-      />
-
       <div id="Result" />
 
-      <Button variant="primary" id="createTopic">
-        Topic{" "}
+      <Button variant="primary" id="createTopic" onClick={addTopic}>
+        Add
       </Button>
       <FormControl type="text" id="topicTextId" defaultValue="Topic" />
-      <Button variant="secondary" id="createIdea">
-        Idea{" "}
-      </Button>
-      <FormControl type="text" id="ideaTextId" defaultValue="Idea" />
     </div>
   );
 };
--- src/Session.js
+++ src/Session.js
@@ -2,20 +2,22 @@ import React from "react";
 import * as d3 from "d3";
 
 const Session = () => {
-  
   var w = window.innerWidth;
   var h = 500;
 
-/*   var svg = d3.select("svg"); //selektiert die svg element mit class (plot) in main.html
- */
-   var svg = d3.select("body")      //variable svg soll eine svg element in body kreieren aber kreiert stattdessen zwei auf Meteor+React
-                  .append("svg")
-                  .attr("width", w)
-                  .attr("height", h);
- 
+  /*   var svg = d3.select("svg"); //selektiert die svg element mit class (plot) in main.html
+   */
+  var svg = d3
+    .select("body") //variable svg soll eine svg element in body kreieren aber kreiert stattdessen zwei auf Meteor+React
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  var connections = [];
 
   function drawCircle(x, y, size) {
     console.log("Drawing circle at", x, y, size);
+
     svg
       .append("circle")
       .attr("class", "click-circle")
@@ -24,8 +26,22 @@ const Session = () => {
       .attr("r", size)
       .style("cursor", "pointer")
       .call(d3.drag().on("drag", dragmove));
-  }
 
+    connections.push({
+      midpoint_x: (x + size) / 2,
+      midPoint_y: (y + size) / 2
+    });
+    console.log(connections)
+  }
+  function drawEdges() {
+    svg
+      .append("line")
+      .style("stroke", "black")
+      .attr("x1", 150)
+      .attr("y1", 100)
+      .attr("x2", 250)
+      .attr("y2", 300);
+  }
 
   function dragmove(d) {
     var x = d3.event.x;
@@ -34,19 +50,14 @@ const Session = () => {
       .attr("cx", x)
       .attr("cy", y);
   }
+
   svg.on("dblclick", function() {
     //double Click to create Circle
-    console.log("Double Click");
     var coords = d3.mouse(this);
-    console.log(coords);
     drawCircle(coords[0], coords[1], 30);
   });
 
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div />;
 };
 
 export default Session;
