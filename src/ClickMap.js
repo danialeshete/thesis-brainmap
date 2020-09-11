e5c7e550d970939f40f36a80c8c1545b1022d712
danialeshete <danial.eshete@gmail.com>
2020-09-11 09:00:17 +0200

    1 file modified

--- src/ClickMap.js
+++ src/ClickMap.js
@@ -21,8 +21,8 @@ const ClickMap = () => {
 
   var width = window.innerWidth,
     height = window.innerHeight - 200,
-    root,
     currentNode,
+    index,
     charLeng,
     padding = 30;
 
@@ -41,144 +41,62 @@ const ClickMap = () => {
     .append("svg")
     .attr("width", width)
     .attr("height", height);
-  const link = svg.append("g").selectAll(".link"), //create Group of  Lines in SVG
-    node = svg.append("g").selectAll("g");
 
-  d3.select("#app")
-    .append("div")
-    .attr("id", "tooltip")
-    .attr("style", "position: absolute; opacity: 0;");
+  var nodes = force.nodes(),
+    links = force.links(),
+    node = svg.selectAll(".node"),
+    link = svg.selectAll(".link"),
+    text = svg.selectAll(".text");
 
   function add() {
     var topic = document.getElementById("topic").value;
-    var size = topic.length * 5;
     if (topic != "") {
-      if (root) {
-        //child
-        var obj = {
-          name: topic,
-          size: size,
-          children: [],
-          colorID: "#3182bd"
-        };
-        currentNode["children"].push(obj);
+      var node = {
+        text: topic,
+        colorID: "#00b4d8"
+      };
+      if (nodes.length > 0) {
+        links.push({ source: node, target: currentNode });
       } else {
-        //parent
-        root = {
-          name: topic,
-          size: size,
-          children: [],
-          colorID: "#a8dadc"
-        };
-        currentNode = root;
+        node.colorID = "#caf0f8";
+        currentNode = node;
       }
+      nodes.push(node);
       update();
       document.getElementById("topic").value = "";
       document.getElementById("topic").placeholder =
         "What are your Ideas to the topic?";
-    } else {
     }
   }
 
-  function collapse(currentNode) {
-    if (currentNode.children) {
-      currentNode._children = currentNode.children;
-      currentNode._children.forEach(collapse);
-      currentNode.children = null;
-    }
-  }
-
-  //root.children.forEach(collapse);
   function update() {
-    var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
+    // Update Circles
+    node = node.data(nodes);
+    node
+      .enter()
+      .insert("circle")
+      .attr("class", "node")
+      .on("click", clickNode)
+      .call(force.drag);
+    node.exit().remove();
 
-    // Restart the force layout.
-    force
-      .nodes(nodes)
-      .links(links)
-      .start();
+    // Update Text
+    text = text.data(nodes);
+    text
+      .enter()
+      .insert("text")
+      .attr("class", "text");
+    text.exit().remove();
 
-    // Update the links…
-    link = link.data(links, function(d) {
-      return d.target.id;
-    });
-
-    // Exit any old links.
-    link.exit().remove();
-
-    // Enter any new links.
+    // Update Links
+    link = link.data(links);
     link
       .enter()
       .insert("line", ".node")
-      .attr("class", "link")
-      .attr("x1", function(d) {
-        return d.source.x;
-      })
-      .attr("y1", function(d) {
-        return d.source.y;
-      })
-      .attr("x2", function(d) {
-        return d.target.x;
-      })
-      .attr("y2", function(d) {
-        return d.target.y;
-      });
+      .attr("class", "link");
+    link.exit().remove();
 
-    // Update the nodes…
-    node = node
-      .data(nodes, function(d) {
-        return d.id;
-      })
-      .style("fill", d => d.colorID);
-
-    // Exit any old nodes.
-    node.exit().remove();
-
-    // Enter any new nodes.
-    node
-      .enter()
-      .append("g")
-      .attr("class", "circle")
-      .append("circle")
-      .attr("class", "node")
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      })
-      .attr("r", function(d) {
-        return d.size;
-      })
-      .style("fill", d => d.colorID)
-      .on("mouseover", function(d) {
-        d3.select("#tooltip")
-          .transition()
-          .duration(200)
-          .style("opacity", 1)
-          .text(d.name);
-      })
-      .on("mouseout", function() {
-        d3.select("#tooltip").style("opacity", 0);
-      })
-      .on("click", clickNode)
-      .call(force.drag);
-
-    node
-      .append("text")
-      .text(function(d) {
-        return d.name;
-      })
-      .attr("x", function(d) {
-        return d.x;
-      })
-      .attr("y", function(d) {
-        return d.y;
-      })
-      .style("fill", "Black")
-      .on("click", clickNode)
-      .call(force.drag);
+    force.start();
   }
 
   function tick() {
@@ -197,130 +115,71 @@ const ClickMap = () => {
       });
 
     node
-      .selectAll("circle")
+      .style("fill", d => d.colorID)
       .attr("cx", function(d) {
         return d.x;
       })
       .attr("cy", function(d) {
         return d.y;
+      })
+      .attr("r", function(d) {
+        return d.text.length * 5;
       });
 
-    node
-      .selectAll("text")
+    text
+      .text(function(n) {
+        return n.text;
+      })
       .attr("x", function(d) {
-        return d.x - d.size;
+        return d.x;
       })
       .attr("y", function(d) {
         return d.y;
       });
   }
 
-  // Color leaf nodes orange, and packages white or blue.
   function color(d) {
-    return d.children.length ? "#a8dadc" : "#a8dadc";
+    return d.children ? "#a8dadc" : "#a8dadc";
   }
 
-  // Toggle children on click.
-  function clickNode(d) {
+  function clickNode(d, i) {
     currentNode = d;
+    index = i;
     document.getElementById("topic").placeholder = `"${
-      currentNode.name
+      currentNode.text
     }" is now active`;
   }
 
-  // Returns a list of all nodes under the root.
-  function flatten(root) {
-    var nodes = [],
-      i = 0;
-    function recurse(node) {
-      if (node.children) node.children.forEach(recurse);
-      if (!node.id || node.id) node.id = ++i;
-      nodes.push(node);
-    }
-
-    recurse(root);
-    return nodes;
-  }
   function changeBG() {
     return document.getElementById("colorPicker").value;
     //document.body.style.backgroundColor = document.getElementById("colorPicker").value;
   }
   function del() {
-    var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
-    var index = nodes.indexOf(currentNode);
-
     nodes.splice(index, 1);
-
     links = links.filter(function(l) {
       return l.source !== currentNode && l.target !== currentNode;
     });
+
+    update();
     document.getElementById("topic").value = "";
-    restart();
+    document.getElementById("topic").placeholder =
+      "What are your Ideas to the topic?";
+    currentNode = nodes[0];
   }
 
   function edit() {
     var topic = document.getElementById("topic").value;
     if (topic != "") {
-      var nodes = flatten(root),
-        links = d3.layout.tree().links(nodes);
       var index = nodes.indexOf(currentNode);
+      nodes[index].text = topic;
 
-      nodes[index].name = topic;
-
-      restart();
+      update();
+      document.getElementById("topic").value = "";
+      currentNode = nodes[index];
+      document.getElementById("topic").placeholder = `"${
+        currentNode.text
+      }" is now active`;
     }
-    document.getElementById("topic").value = "";
-  }
-  function restart() {
-    var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
-    // Update the links…
-    link = link.data(links, function(d) {
-      return d.target.id;
-    });
-
-    // Exit any old links.
-    link.exit().remove();
-
-    // Enter any new links.
-    link
-      .enter()
-      .insert("line", ".node")
-      .attr("class", "link");
-
-    // Update the nodes…
-    node = node
-      .data(nodes, function(d) {
-        return d.id;
-      })
-      .style("fill", d => d.colorID);
-
-    // Exit any old nodes.
-    node.exit().remove();
-
-    // Enter any new nodes.
-    node
-      .enter()
-      .append("g")
-      .attr("class", "circle")
-      .append("circle")
-      .attr("class", "node")
-      .on("click", clickNode)
-      .call(force.drag);
-
-    node
-      .append("text")
-      .text(function(d) {
-        return currentNode.name;
-      })
-      .call(force.drag);
-
-    // Restart the force layout.
-    force
-      .nodes(nodes)
-      .links(links)
-      .start();
   }
 
   return (
